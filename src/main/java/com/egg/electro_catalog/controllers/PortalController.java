8ebc891bde2f62cd8d03a4cffb13049b4e123827
Fernando Noguera <jfnoguerab@gmail.com>
2025-03-18 17:42:28 -0500

    Se agregaron los endpoints para el login (inicio de sesión y registro) en el controlador 'PortalController' y se crearon las plantillas correspondientes

--- src/main/java/com/egg/electro_catalog/controllers/PortalController.java
+++ src/main/java/com/egg/electro_catalog/controllers/PortalController.java
@@ -1,7 +1,16 @@
 package com.egg.electro_catalog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.egg.electro_catalog.model.dtos.UsuarioCreateDTO;
+import com.egg.electro_catalog.model.entities.Usuario;
+import com.egg.electro_catalog.model.enums.Rol;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -12,6 +21,25 @@ public class PortalController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/registrar")
+    public String registrar(Model model) {
+        model.addAttribute("usuario", new UsuarioCreateDTO());
+        return "login/form";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, Model model) {
+        try {
+            if (error != null) {
+                model.addAttribute("error", "Usuario y/o contraseña incorrectos.");
+            }
+            return "login/index";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "login/index";
+        }
     }
     
 }
